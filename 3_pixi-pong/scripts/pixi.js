36f028ab7211d7fb374c0dfa9fec7f7b665e12cd
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-01-24 15:28:41 -0500

    JavaScript: commented out xode to debug issues

--- 3_pixi-pong/scripts/pixi.js
+++ 3_pixi-pong/scripts/pixi.js
@@ -38,21 +38,13 @@ let yv = 1
 // ___________   RECTANGLE BORDER IMAGES
 
 
-
-// ____ Each Boarder will be colored a different color than the blue background
-
 //  Object Rectangle Properties
-
-
 // Create And Define the Border Properties
 // Name, x, y, width, height, default color, hit color
 const borders = [
     {name: "top", x: 0, y: 0, width: 800, height: 5, color: 0xffffff, hitColor: 0xff00000},
-    
     {name: "bottom", x: 0, y: 795, width: 795, height: 5, color: 0xffffff, hitColor: 0x0aefff},
-   
     {name: "left", x: 0, y: 0, width: 5, height: 795, color: 0xffffff, hitColor: 0xff9a00},
-   
     {name: "right", x: 795, y: 0, width: 5, height: 800, color: 0xffffff, hitColor: 0xffdd00},
 ];
 
@@ -60,25 +52,14 @@ const borders = [
 // Contain the border images from the border loop 
 const borderImage = {};
 
-// ____ Create 4 Rectangles
-
-// For Each Loop 
-// 1 - Create new graphic
-// 2 - Draw graphic
-// 3 - Append to to stage 
-
-
-
 // Draw 4 rectangles and append to the stage
 borders.forEach(border => {
 
 // Instantiate a new graphic image of a rectangle
 const rect = new PIXI.Graphics();
-
 rect.beginFill(border.color);
 rect.drawRect(0,0, border.width, border.height);
 rect.endFill();
-
 
 // Set the cordinates of the rectangls on the canvas
 rect.x = border.x;
@@ -90,8 +71,6 @@ app.stage.addChild(rect);
 // Pass all the borders to the borderImage object
 borderImage[border.name] = rect;
 
-
-
 })
 
 
@@ -101,21 +80,21 @@ const hitBorders = new Set();
 
 // Show Hit Borders by changing their color
 
-function showHitBorder(name) {
+// function showHitBorder(name) {
 
-    // Find the border properties in the border object array
-    const border = borders.find(b => b.name === name);
+//     // Find the border properties in the border object array
+//     const border = borders.find(b => b.name === name);
 
-    // Get the border object - clear and change color to show as hit
-    if (border) {
-        const rect = borderGraphics[name];
-        rect.clear();
-        rect.beginFill(border.hitColor);
-        rect.drawRect(0,0, border.width, border.height);
-        rect.endFill();         
-    }
+//     // Get the border object - clear and change color to show as hit
+//     if (border) { 
+//         const rect = borderImage[name];
+//         rect.clear();
+//         rect.beginFill(border.hitColor);
+//         rect.drawRect(0,0, border.width, border.height);
+//         rect.endFill();         
+//     }
 
-}
+// }
 
 
 
@@ -132,22 +111,26 @@ function trackHits() {
     // Set Conditinals for each border to check if hit
 
     //  Track Top Border
-    if(circle.y - 6 <= 5) {
+    if(circle.y - 6 <= 6 && !hitBorders.as("top")) {
+        // showHitBorder("top");
         lastBorder = "top";
-        showHitBorder("top");
+        hitBorders.add("top");
+       
     }
 
     // Track Botton Border
     if(circle.y + 6 >= 795) {
         lastBorder = "bottom";
-        showHitBorder("botto");
+        // showHitBorder("botto");
+        hitBorders.add("bottom");
 
     }
 
     // Track Left Border
     if(circle.x - 6 <= 5) {
         lastBorder = "left";
-        showHitBorder("left");
+        // showHitBorder("left");
+        hitBorders.add("left");
 
     }
 
@@ -155,7 +138,21 @@ function trackHits() {
     if(circle.x + 6 >= 795) {
         lastBorder = "right";
         showHitBorder("right");
+        hitBorders.add("right");
     }
+
+    // return new Promise((resolve, reject) => {
+    //     if (hitBorders.size === 4) {
+    //         if (lastBorder) showHitBorder(lastBorder);
+    //         resolve("All borders have been hit!") // Resolve
+    //     }
+    //     else {
+    //         reject(); // Nothing to reject ? ____  Check This _____
+    //     }
+
+
+    // })
+
 
 
 }
@@ -227,30 +224,18 @@ app.ticker.add(() => {
 // Create a promis
 
 
-const promise = () => {
+// const promise = () => {
 
-    return new Promise((resolve, reject) => {
-        if (hitBorders === 4) {
-            resolve("All borders have been hit!") // Resolve
-        }
-        else {
-            reject(); // Nothing to reject ? ____  Check This _____
-        }
-
-
-    })
-
-
-}
+// }
 
 // Promis Resolve Check and Alert if - All 4 borders have been hit
-promiseCheck(message)// Resolve - Then: stop game and notify user
-    .then(message => {
-        // Stop the game
-        app.ticker.stop();
-        alert(message);
-    })
+// promiseCheck(message)// Resolve - Then: stop game and notify user
+//     .then(message => {
+//         // Stop the game
+//         app.ticker.stop();
+//         alert(message);
+//     })
 
-    .catch(() => {
-        console.error("Error");
-    });
+//     .catch(() => {
+//         console.error("Error");
+//     });
