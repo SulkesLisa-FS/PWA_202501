4cb9317a4a795a72a8d7a461cb7be67b31cd8930
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-01-25 14:43:56 -0500

    JavaScript: cleaned up code and checked spelling and grammer.

--- 3_pixi-pong/scripts/pixi.js
+++ 3_pixi-pong/scripts/pixi.js
@@ -8,24 +8,20 @@ await app.init({
     backgroundColor: 0x164555, width: 800, height: 800 });
 // Append the canvas to the DOM body
 document.body.appendChild(app.canvas);
-// document.querySelector("#game").appendChild(app.canvas);
-
 
 // ___________  CIRCLE
 // Draw the Circle Image 
 const circle = new PIXI.Graphics();
 circle.beginFill(0xf5ef42);
-circle.drawCircle(0, 0, 6);
+circle.drawCircle(0, 0, 8);
 circle.endFill();
-// Append the circle to the Canvas Stage
-app.stage.addChild(circle);
-// Initial Cordinates of the Circle on the Canvas - Near the center
+app.stage.addChild(circle);// Append the circle to the Canvas Stage
+// Initial Coordinates of the Circle on the Canvas - Near the center
 circle.x = 450;
 circle.y = 350;
 // Set the Circle's Velocity & Change of Direction
 let xv = 1.5;
 let yv = 1;
-
 
 // ___________   RECTANGLE BORDERS
 //  Object Rectangle Properties
@@ -36,126 +32,94 @@ const borders = [
     {name: "left", x: 0, y: 0, width: 3, height: 795, color: 0xffffff, hitColor: 0xff9a00},
     {name: "right", x: 795, y: 0, width: 3, height: 795, color: 0xffffff, hitColor: 0xffdd00},
 ];
-// Contain the border images from the border loop 
-const borderImage = {};
+const borderImage = {};// Contain the border images from the border loop 
 // Draw 4 rectangles and append to the stage
 borders.forEach(border => {
-// Instantiate a new graphic image of a rectangle
-const rect = new PIXI.Graphics();
+const rect = new PIXI.Graphics();// Instantiate a new graphic image of a rectangle
 rect.beginFill(border.color);
 rect.drawRect(0,0, border.width, border.height);
 rect.endFill();
-// Set the cordinates of the rectangls on the canvas
+// Set the coordinates of the rectangles on the canvas
 rect.x = border.x;
 rect.y = border.y;
-// Append the border to the stage
-app.stage.addChild(rect);
+app.stage.addChild(rect);// Append the border to the stage
 // Pass all the borders to the borderImage object
-borderImage[border.name] = rect;
-});
+borderImage[border.name] = rect;});
 
 // ___________   SHOW BORDERS
-// Keep Track of What borders where hit
-const hitBorders = new Set();
-// Show Hit Borders by changing their color
-const showHitBorder = (name) =>  {
+const hitBorders = new Set(); // Keep Track of What borders where hit
+const showHitBorder = (name) =>  {// Show Hit Borders by changing their color
     // Find the border properties in the border object array
     const border = borders.find(b => b.name === name);
-    // Take the hit border - clear and change color 
-    if (border) { 
+    if (border) {  // Take the hit border - clear and change color 
         const rect = borderImage[name];
         // Clear Rectangle Image - Redraw and give new color to show the border was hit. 
         rect.clear();
         rect.beginFill(border.hitColor);
         rect.drawRect(0,0, border.width, border.height);
-        rect.endFill();         
-    };
-};
+        rect.endFill();};};
 
-// ___________   SHOW BORDERS
-// CHECK if the 4 borders has been hit and call showHitBorder function
-
+// ___________  TRACK BORDERS
+// CHECK if the 4 borders have been hit and call showHitBorder function
 const trackHits =() => {
-    // Check if Last Border - Set value as undefined.
-    let lastBorder = null;
-    // Set Conditinals for each border to check if hit
+    let lastBorder = null; // Check if Last Border - Set value as undefined.
+    // Set Conditionals  for each border to check if hit
     //  Track Top Border
     if(circle.y - 6 <= 5 && !hitBorders.has("top")) {
-        // Add hit border first - then show 
-        hitBorders.add("top");
-        showHitBorder("top");
-        // Change lastBorder Value
-        lastBorder = "top";
-    }
+        hitBorders.add("top"); // Add hit border first - then show 
+        showHitBorder("top"); // Change lastBorder Value
+        lastBorder = "top";}
     // Track Botton Border
     if(circle.y + 6.5 >= 795 && !hitBorders.has("bottom")) {
         hitBorders.add("bottom");
         lastBorder = "bottom";
-        showHitBorder("bottom");
-    }
+        showHitBorder("bottom");}
     // Track Left Border
     if(circle.x - 6 <= 5 && !hitBorders.has("left")) {
         hitBorders.add("left");
         lastBorder = "left";
-        showHitBorder("left");
-    }
+        showHitBorder("left");}
     // Track Right Border
     if(circle.x + 6.5 >= 795 && !hitBorders.has("right")) {
         hitBorders.add("right");
         lastBorder = "right";
-        showHitBorder("right");
-    }
+        showHitBorder("right");}
     //  Return a promise to resolve when all four borders are hit.
     return new Promise((resolve, reject) => {
-
         if (hitBorders.size === 4) {
             // Highlight Boarder Before  the Alert
             if (hitBorders) showHitBorder(lastBorder);
             // Resolve After border color change - When all four borders are hit
             // Resolve - Message 
-            setTimeout(() => resolve("All borders have been hit!"), 100);  
-        }
+            setTimeout(() => resolve("All borders have been hit!"), 100);}
         else {
-            // Will continue as un-resolved. 
-            reject(); 
-        }
-    });
-};
-// Variable to show and not show the Resolved Promis Alert
-// To fix the infinate loop and show only one time.
+            // Will continue as unresolved. 
+            reject();}});};
+// Variable to show and not show the Resolved Promise Alert
+// To fix the infinite loop and show only once.
 let showAlert = true;
 // Add Motion to the Circle
 app.ticker.add(() => {
-    // Check if Circle radious X goes past 800px  or is less than zero
+    // Check if Circle adius  X goes past 800px or is less than zero
     if (circle.x + 6.5 >= 800 || circle.x - 6.5 <= 0) {
         // Then reverse direction to negative number
-        xv = -xv;
-    }
-     // Check if Circle radious Y goes past 800px  or is less than zero
+        xv = -xv;}
+     // Check if Circle radius  Y goes past 800px or is less than zero
      if (circle.y + 6.5 >= 800 || circle.y - 6.5 <= 0) {
         // Then reverse direction to negative number
-        yv = -yv;
-    }
+        yv = -yv;}
     // Move the Circle by its direction/velocity
     circle.x += xv;
     circle.y += yv; 
-    // Call Track Hits Funcion
-    trackHits()
-        // Resolve = Then: stop game      and notify user
-        .then(message => {
-             // Stop the game
-            app.ticker.stop();
-            // Check if showAlert is True
-            if (showAlert === true) {
-                // Resolve - Alert user that all borders have been hit
-                alert(message);
-                // Change ShowAlert to False - to not show again
-                showAlert = false;
-            }
-        })
+    
+    trackHits() // Call Track Hits Function     
+        .then(message => { // Resolve = Then: stop game 
+            app.ticker.stop(); // Stop the game
+            if (showAlert === true) { // Check if showAlert is True
+                alert(message); // Resolve - Alert user that all borders have been hit
+                showAlert = false;}})   // Change ShowAlert to False - to not show again       
         .catch(() => {
             // console.error("Promise Not Resolved Yet");
             // Catch is to handle the rejected promise
-            // The promise will be rejected untill all four borders are hit.   
-        });
-});
+            // The promise will be rejected until all four borders are hit.   
+        });});
