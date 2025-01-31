d378f2200bb400531e80fafe876b0686dba70c29
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-01-31 13:54:16 -0500

    script.test.js: added sumOfArray function to module import. added test function for sumOfArray() an first test to add integers for a sum.

--- 4_test_codebase/script.test.js
+++ 4_test_codebase/script.test.js
@@ -1,7 +1,19 @@
-const {sum} = require('./script'); 
+const {sum, sumOfArray} = require('./script'); 
 
 // TEST - IMPORT AND EXPORT OF FUNCIONS
 test('Test sum: add 5 + 10 equals 15', () => {
   expect(sum(5, 10)).toBe(15);
 });
 
+// FUNCTION ONE ______________________________
+
+test('Test sumOfArray() - Returns the sum of all numbers', () => {
+
+  // Expect Array of 2, 4, 6, 2 to Return the sum number 14
+  expect(sumOfArray([2, 4, 6, 2])).toBe(14);
+
+
+
+
+
+})
