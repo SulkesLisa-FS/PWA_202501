01b4425fe7ed45ad3b9bae70edfee9768d5a4d87
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-01-31 14:25:07 -0500

    script.test.js: sumOfArrays - added test for negative numbers and test for  negative decimals.

--- 4_test_codebase/script.test.js
+++ 4_test_codebase/script.test.js
@@ -11,8 +11,12 @@ test('Test sumOfArray() - Returns the sum of all numbers', () => {
 
   // Expect Array of 2, 4, 6, 2 to Return the sum number 14
   expect(sumOfArray([2, 4, 6, 2])).toBe(14);
+  // Expect Arra of Negative numbers -2, -4, -6 to Return the sum of -12
+  expect(sumOfArray([-2, -4, -6])).toBe(-12)
   // Expect Floating point 2.5, 3.5, 1.3 to Return Close To 7.3
   expect(sumOfArray([2.5, 3.5, 1.3])).toBeCloseTo(7.3);
+  // Expect Negative Floating point -2.5, -3.5, -1.3 to Return Close To -7.3
+  expect(sumOfArray([-2.5, -3.5, -1.5])).toBeCloseTo(-7.3);
   
 
 
