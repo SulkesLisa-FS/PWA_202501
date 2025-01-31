c97ef2ca5ea53190d19392528624b67961bcc874
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-01-31 14:18:24 -0500

    script.test.js: added test for array of decimal integers.

--- 4_test_codebase/script.test.js
+++ 4_test_codebase/script.test.js
@@ -11,6 +11,9 @@ test('Test sumOfArray() - Returns the sum of all numbers', () => {
 
   // Expect Array of 2, 4, 6, 2 to Return the sum number 14
   expect(sumOfArray([2, 4, 6, 2])).toBe(14);
+  // Expect Floating point 2.5, 3.5, 1.3 to Return Close To 7.3
+  expect(sumOfArray([2.5, 3.5, 1.3])).toBeCloseTo(7.3);
+  
 
 
 
