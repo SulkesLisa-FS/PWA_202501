1e483279641c02291b9ab097e4c1f2c16bb73a22
Lisa Sulkes <lpsulkes@student.fullsail.edu>
2025-01-31 16:08:19 -0500

    script.js: removed array varible - was used for testing function only.

--- 4_test_codebase/script.js
+++ 4_test_codebase/script.js
@@ -10,8 +10,6 @@ const sumOfArray = (arr) => arr.reduce((sum, num) => sum + num, 0);
 
 // FUNCTION TWO ______________________________
 
-let arr = [22, 43, 55, 99]
-
 // Takes an array of numbers and returns the larges number
 const findMax = (arr) => Math.max(...array)
 
